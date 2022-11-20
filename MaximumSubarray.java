3e5b14c025c16e355d81cc7481f6ce863e2e8fb8
Piyush Sahay <p1301sahay@gmail.com>
2022-11-20 11:02:11 +0530

    Update MaximumSubarray.java

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -9,7 +9,7 @@ class MaximumSubarray {
         int maxl=0;
         int maxg= Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
-            if(maxl+nums[i]<nums[i])
+            if(maxl+nums[i]<nums[i]) //Condition to handle for array conataiing both +ve and -ve number check if local+num[i] is lees than num[i] then that cannot be max subarray
                 maxl=nums[i];
             else
                 maxl=maxl+nums[i];
